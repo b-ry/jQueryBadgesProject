0e84e1ba9ea74f521990fa8a157963ac3b0a5b96
bryon <bryon@newmediadenver.com>
2017-09-22 14:45:24 -0600

    Ajax call to get badges for completed courses.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
-  // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/bryonurbanec.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      for (var x = 0; x < response.courses.completed.length; x++) {
+        var div = $("<div>").attr("class", "course");
+        $(div).append($("<h3>").html(response.courses.completed[x].title));
+        $(div).append($("<img>").attr("src", response.courses.completed[x].badge));
+        $(div).append($("<a>").attr("href", response.courses.completed[x].url)
+          .attr("target", "_blank")
+          .addClass("btn btn-primary")
+          .html("See Course")
+        );
+        $("#badges").append(div);
+      }
+    }
+  });
 });
